e8cba3864e6e5d67bc43d88cb69609a70d150d61
Lilian Ávila <lilavila.dev@gmail.com>
2023-10-27 01:19:32 -0300

    Validação fors com Zod e início de estilização

--- src/components/Footer/index.jsx
+++
@@ -1,9 +0,0 @@
-export const Footer = () => {
-    return (
-        <footer>
-            <p>
-                &copy;Todos os direitos reservados - Kenzie Academy Brasil
-            </p>
-        </footer>
-    );
-};
--- src/pages/RegisterPage/index.jsx
+++
@@ -1,9 +0,0 @@
-import { RegisterForm } from "../../components/forms/RegisterForm";
-
-export const RegisterPage = () => {
-    return(
-    <main>
-        <h1>Página de registro</h1>
-        <RegisterForm />
-    </main>)
-};
